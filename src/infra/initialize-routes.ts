1542b095edc18bd8d52823e2d049b6046ead5c73
Antonio Lemos <antoniobilbylemos@gmail.com>
2023-11-26 21:18:22 -0300

    remove authenticatino from proxy

--- src/infra/initialize-routes.ts
+++ src/infra/initialize-routes.ts
@@ -31,7 +31,6 @@ export function initializeRoutes() {
 
   app.get(
     '/:externalId/thumbnail',
-    ensureAuthenticated,
     validateSchema(Videos.getCdnResources),
     videosController.getThumbnail.bind(videosController),
   );
